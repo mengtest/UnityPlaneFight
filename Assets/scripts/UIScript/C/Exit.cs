1da3c144a7b8a8f349d5343dd42fa107cb487d52
sangliang <378305868@qq.com>
2015-03-20 15:30:10 +0800

    原始代码转换完

---
+++ Assets/scripts/UIScript/C/Exit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exit : MonoBehaviour {
+    public GUISkin mySkin;
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      
+	}
+
+    void OnGUI() {
+        GUI.skin = mySkin;
+        if (GUI.Button(new Rect(190f, 280f, 100f, 40f), "", GUI.skin.GetStyle("exitButton")))
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/scripts/UIScript/C/Result.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Result : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.guiText.text = "Your Score is " + GameObject.Find("score").GetComponent<scoreCount>().score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
